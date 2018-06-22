01d483b7ceaa9d2a6ffdd02d82c9a440007bab2c
moumy <mmy29279@outlook.com>
2018-06-22 21:00:32 +0800

    gitignore and init configuration

---
+++ src/test/java/com/graduation/gateway/zuulext/ZuulExtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.graduation.gateway.zuulext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulExtApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
